e0c16743a73a429ce28d2787b9af1c4f6ba4689a
sami27avila <avilasamu27@gmail.com>
2024-11-24 04:39:54 +0100

    menu.php
    Proyecto

---
+++ menu.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <a class="navbar-brand">oddisey Web</a>
+    <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div id="my-nav" class="collapse navbar-collapse">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled" href="#">Carrito(0)</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<footer class="footer footer-expand-lg footer-dark bg-dark fixed-bottom">
+   <ul class="footer-link">
+      <li class="list-link">
+          <a class="links" href="#">Terminos y condiciones</a>
+      </li>
+      <li class="list-link">
+          <a class="links" href="#">Accesibilidad</a>
+      </li>
+   </ul>
+   <span class="text-footer">Copyright © 2024 Oddisey Web</span>
+</footer>
+
